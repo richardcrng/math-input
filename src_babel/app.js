7f1cc73d48274826948bb507d47ed440260f8a2a
Richard Ng <richardcrng@gmail.com>
2019-01-14 17:41:41 +0000

    reorganise compilation

--- src_babel/app.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var React = require('react');
-var ReactDOM = require('react-dom');
-
-var App = require('./components/app');
-
-ReactDOM.render(React.createElement(App, null), document.getElementById('root'));
