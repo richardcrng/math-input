85bdb308041b340a72d51fc893d22a8c9b9f77c3
Richard Ng <richardcrng@gmail.com>
2019-01-15 11:30:21 +0000

    try handling on desktop

--- src_raw/components/input/math-input.js
+++ src_raw/components/input/math-input.js
@@ -704,7 +704,10 @@ class MathInput extends React.Component {
             onTouchStart={this.handleTouchStart}
             onTouchMove={this.handleTouchMove}
             onTouchEnd={this.handleTouchEnd}
-            onClick={e => e.stopPropagation()}
+            onClick={e => {
+              this.handleTouchStart(e);
+              this.handleTouchEnd(e);
+            }}
             role={'textbox'}
             ariaLabel={i18n._('Math input box')}
         >
