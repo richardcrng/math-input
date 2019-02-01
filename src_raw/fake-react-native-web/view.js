625655639ed8dcde1c522478760471118f9bfaca
Richard Ng <richardcrng@gmail.com>
2019-02-01 17:19:36 +0000

    add onMouseMove handler to View

--- src_raw/fake-react-native-web/view.js
+++ src_raw/fake-react-native-web/view.js
@@ -70,6 +70,7 @@ class View extends React.Component {
             style={this.props.dynamicStyle}
             onClick={this.props.onClick}
             onMouseDown={this.props.onMouseDown}
+            onMouseMove={this.props.onMouseMove}
             onMouseUp={this.props.onMouseUp}
             onTouchCancel={this.props.onTouchCancel}
             onTouchEnd={this.props.onTouchEnd}
